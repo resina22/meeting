4c10f9c9ef1575e2cc62237a5254bbf211bc3d29
Washington Rodrigues <washingtonjunio16@gmail.com>
2020-02-08 13:06:46 -0300

    Base layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { OnInit, OnDestroy, Component, AfterViewInit } from '@angular/core';
+import {
+  Router,
+  NavigationStart,
+  NavigationCancel,
+  NavigationEnd
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'dating';
+export class AppComponent  implements OnInit, OnDestroy, AfterViewInit {
+  public loading = false;
+  private loadingSubscribe;
+
+  constructor(
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.loading = true;
+  }
+
+  ngOnDestroy() {
+    this.loadingSubscribe.unsubscribe();
+  }
+
+  ngAfterViewInit() {
+    this.loadingSubscribe = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.loading = true;
+      } else if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel
+      ) {
+        this.loading = false;
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FlexLayoutModule,
     BrowserAnimationsModule,
+    MatProgressSpinnerModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
