96b201c850e329b5207c1a4f03eba60bcad5ba63
Washington Rodrigues <washingtonjunio16@gmail.com>
2020-02-15 01:31:40 -0300

    Renomed project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'dating'`, () => {
+  it(`should have as title 'meeting'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('dating');
+    expect(app.title).toEqual('meeting');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('dating app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('meeting app is running!');
   });
 });
