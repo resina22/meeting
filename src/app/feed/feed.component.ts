7a520a6b61e8c3de089748686560b58eb535b082
Washington Rodrigues <washingtonjunio16@gmail.com>
2020-02-15 01:24:07 -0300

    Adjusted feed component

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -6,13 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feed.component.scss']
 })
 export class FeedComponent implements OnInit {
-  imgs = [
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/44_-_Iguazu_-_D%C3%A9cembre_2007.jpg/1920px-44_-_Iguazu_-_D%C3%A9cembre_2007.jpg',
-    'https://www.clickriomafra.com.br/umbanda/wp-content/uploads/2011/07/natureza1.jpg',
-    'https://www.relaxar.org/wp-content/uploads/2019/03/relaxar-natureza.jpg',
-    'https://www.clickriomafra.com.br/umbanda/wp-content/uploads/2011/07/natureza1.jpg',
-  ];
-
   constructor() {}
 
   ngOnInit() {}
--- src/app/feed/feed.module.ts
+++ src/app/feed/feed.module.ts
@@ -7,14 +7,24 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { FeedComponent } from './feed.component';
+import { SharedModule } from '../shared/shared.module';
+import { MediaComponent } from './media/media.component';
 import { FeedRoutingModule } from './feed-routing.module';
+import { ActionComponent } from './action/action.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    FeedComponent
+    FeedComponent,
+    MediaComponent,
+    ActionComponent,
+    ContentComponent,
+    ProfileComponent,
   ],
   imports: [
     CommonModule,
+    SharedModule,
     MatIconModule,
     MatButtonModule,
     FlexLayoutModule,
