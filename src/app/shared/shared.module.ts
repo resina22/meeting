56098fc87d69e02e54c0ccff3f7d38d1207901a1
Washington Rodrigues <washingtonjunio16@gmail.com>
2020-02-22 16:23:52 -0300

    Created component float button

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,22 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { LoadComponent } from './load/load.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { FloatButtonComponent } from './float-button/float-button.component';
 
 @NgModule({
   declarations: [
-    LoadComponent
+    LoadComponent,
+    FloatButtonComponent
   ],
   imports: [
     CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    AppRoutingModule,
+    MatTooltipModule,
     MatProgressSpinnerModule,
   ],
   exports: [
+    LoadComponent,
     MatDialogModule,
-    LoadComponent
+    FloatButtonComponent,
   ]
 })
 export class SharedModule { }
